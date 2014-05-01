2873b6e6a912f456e147c6288d046fa3a18f45ec
Calvin Tee <calvin@collectskin.com>
2014-05-01 14:39:46 +0800

    styled 3 pages

--- assets/main.js
+++ assets/main.js
@@ -1,12 +1,28 @@
 $(document).ready(function() {
-	$('#fullpage').fullpage({
-		verticalCentered: true,
-		navigation: true,
-		continuousVertical: false,  
-	});
-  $('.scene').parallax();
-  $(document).resize(function(){
-    $("#gmap").width($(this).width());
-    $("#gmap").height($(this).height());
-  }).trigger("resize")
+ function init(){
+	      $('#fullpage').fullpage({
+		      verticalCentered: true,
+		      navigation: true,
+		      continuousVertical: false,
+          slidesNavigation: true,
+          slidesNavPosition: 'bottom', 
+	      });
+        $('.scene').parallax();
+        $(document).resize(function(){
+          $("#gmap").width($(this).width());
+          $("#gmap").height($(this).height());
+        }).trigger("resize");
+ }
+  var feed = new Instafeed({
+      get: 'tagged',
+      tagName: 'thesaladbarmy',
+      clientId: '107c09c4ca70419c9e37c6844c75bafc',
+      template: '<div class="slide"><a class="th radius" href="{{link}}"><img class="instagram" src="{{image}}" /></a></div>',
+      success: init,
+      error: init,
+		  resolution: 'low_resolution'
+  });
+  feed.run();
+
 });
+
